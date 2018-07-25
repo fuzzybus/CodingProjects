f113ce0030c69a0e870ca93ffa6635b7c59e16b8
fuzzybus <alan.diepraam@gmail.com>
2018-07-25 15:07:53 +1200

    Update FirstSchema.scm

--- FirstSchema.scm
+++ FirstSchema.scm
@@ -2,23 +2,26 @@
 
 riskCalc();
 vars
-		player_1: Decimal [4,2]; player_2: Decimal [4,2];
-		spread: Decimal [4,2]; bet: Decimal [7,2]; winnings: Decimal [7,2];
-		action_1: String; action_2: String;
+	player_1: Decimal [4,2]; player_2: Decimal [4,2];
+	spread: Decimal [4,2]; bet: Decimal [7,2]; winnings: Decimal [7,2];
+	action_1: String; action_2: String;
 begin
-		read player_1; read player_2; read bet;
-		winnings := bet * player_1;
-		spread := player_1 - player_2;
+	read player_1; read player_2; read bet;
+	winnings := bet * player_1;
+	spread := player_1 - player_2;
+		
 		if player_2 > player_1
-				then spread := - (spread);
+			then spread := - (spread);
 		endif;
 		write "Risk factor is equal to: " & spread.display;
+		
 		if (spread >= 1.40) and (player_1 <= 1.40)
-				then action_1 := "Let it ride! Potential winnings are: $" & winnings.display;
-				write action_1;
+			then action_1 := "Let it ride! Potential winnings are: $" & winnings.display;
+		write action_1;
 		endif;
+		
 		if (spread <= 1.39) or (player_1 >= 1.41) 
-				then action_2 := "Hold your horses, no bet! Potential loss is: $" & bet.display;
-				write action_2;
+			then action_2 := "Hold your horses, no bet! Potential loss is: $" & bet.display;
+			write action_2;
 		endif;
 end;
